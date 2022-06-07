f592a6ca386ea18d43a9107cefec7a656df5eb02
Safidy <robustemmanuel@gmail.com>
2022-06-07 18:35:33 +0300

    Interface registration V0.7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import ForgotPassword from "./components/pages/Forgot-Password"
 import Login from "./components/pages/Login"
+import Register from "./components/pages/Register"
 function App() {
 	
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Login/>}/>
 				<Route path="/forgot-password" element={<ForgotPassword/>}/>
+				<Route path="/register" element={<Register/>}/>
 			</Routes>
 		</BrowserRouter>
 	)
