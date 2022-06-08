c3c2c289e2a4397f9aea25793dd02fc3a915d41a
Safidy <robustemmanuel@gmail.com>
2022-06-08 16:34:41 +0300

    Configuring the protected route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import ForgotPassword from "./components/pages/Forgot-Password"
-import Login from "./components/pages/Login"
-import Register from "./components/pages/Register"
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import ProtectedRoute from './components/atoms/ProtectedRoute'
+import Dashboard from './components/pages/Dashboard'
+import ForgotPassword from './components/pages/Forgot-Password'
+import Login from './components/pages/Login'
+import Register from './components/pages/Register'
 function App() {
-	
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Login/>}/>
-				<Route path="/forgot-password" element={<ForgotPassword/>}/>
-				<Route path="/register" element={<Register/>}/>
+				<Route path='/' element={<Login />} />
+				<Route path='/forgot-password' element={<ForgotPassword />} />
+				<Route path='/register' element={<Register />} />
+				<Route element={<ProtectedRoute authorize={false}/>}>
+					<Route path='/dashboard' element={<Dashboard/>}/>
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	)
---
+++ src/components/atoms/ProtectedRoute.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Navigate, Outlet} from 'react-router-dom'
+
+const ProtectedRoute = ({authorize, redirect = '/', children}) => {
+	if (!authorize) {
+		return <Navigate to={redirect} replace />
+	}
+	return children ? children : <Outlet />
+}
+
+export default ProtectedRoute
